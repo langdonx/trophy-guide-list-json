93592152feacb1dc5fc2439993da5aae5ebcfefe
langdonx <441982+langdonx@users.noreply.github.com>
2025-07-11 18:31:58 -0400

    guides now contain gameIds to aid with indicating progress

--- types/guides-v2.d.ts
+++ types/guides-v2.d.ts
@@ -1,6 +1,7 @@
 export interface Guide {
     a: number // attributes, see README
     d: number // published, sometimes date scraped
+    g: number[] // gameIds that this guide applies to (the first in the list is the game/stack the guide was written for)
     i?: string // image
     n: string // name of guide
     p?: string // path
